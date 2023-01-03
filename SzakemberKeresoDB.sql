ccf871bd06d0b31473af76b9701abee6c8f90114
Kapusbenjamin <47866619+Kapusbenjamin@users.noreply.github.com>
2023-01-03 19:06:28 +0100

    Tárolt eljárások összegyűjtése és db legenerálása
    Előzőleg elkészített dokumentumok hozzáadása. Külső forrásból adatbázis formában adatok mellékelve későbbi importáláshoz.

--- SzakemberKeresoDB.sql
+++ SzakemberKeresoDB.sql
@@ -1,159 +1,433 @@
-CREATE TABLE `users` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `first_name` varchar(100),
-  `last_name` varchar(100),
-  `acces_type` varchar(100),
-  `email` varchar(200),
-  `phone` varchar(12),
-  `password` varchar(255),
-  `company_id` int(11),
-  `job_tag_id` int(11),
-  `status` int,
-  `last_login_at` timestamp,
-  `created_at` timestamp,
-  `activated_at` timestamp,
-  `updated_at` timestamp,
-  `address_id` int(11),
-  `deleted` int(1)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2023. Jan 03. 19:03
+-- Kiszolgáló verziója: 10.4.24-MariaDB
+-- PHP verzió: 7.4.29
 
-CREATE TABLE `jobs` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `customer_id` int(11),
-  `worker_id` int(11),
-  `desc` text,
-  `total` int,
-  `status` int,
-  `worker_accepted` int(1),
-  `customer_accepted` int(1),
-  `updated_at` timestamp,
-  `deleted` int(1)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `companies` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(200),
-  `premise_address` varchar(255),
-  `tax_number` varchar(255)
-);
 
-CREATE TABLE `ads` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int(11),
-  `job_tag_id` int(11),
-  `desc` text,
-  `county_id` int(11),
-  `updated_at` timestamp,
-  `status` int(1),
-  `deleted` int(1)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `counties` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(100)
-);
+--
+-- Adatbázis: `szakemberkereso`
+--
+CREATE DATABASE IF NOT EXISTS `szakemberkereso` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `szakemberkereso`;
 
-CREATE TABLE `cities` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `postal_code` int(5),
-  `city` varchar(100),
-  `county_id` int(11)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `streets` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `street_name_id` int(11),
-  `city_id` int(11)
-);
+--
+-- Tábla szerkezet ehhez a táblához `addresses`
+--
 
+DROP TABLE IF EXISTS `addresses`;
 CREATE TABLE `addresses` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `number` varchar(30),
-  `staircase` varchar(30),
-  `floor` int(4),
-  `door` int(8),
-  `street_id` int(11)
-);
+  `id` int(11) NOT NULL,
+  `number` varchar(30) NOT NULL,
+  `staircase` varchar(30) DEFAULT NULL,
+  `floor` int(4) DEFAULT NULL,
+  `door` int(8) DEFAULT NULL,
+  `street_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `street_names` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(100)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `job_tags` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(100)
-);
+--
+-- Tábla szerkezet ehhez a táblához `ads`
+--
 
-CREATE TABLE `messages` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `sender_id` int(11),
-  `receiver_id` int(11),
-  `message` text,
-  `checked` int(1),
-  `sended_at` timestamp,
-  `deleted` int(1)
-);
+DROP TABLE IF EXISTS `ads`;
+CREATE TABLE `ads` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `job_tag_id` int(11) NOT NULL,
+  `desc` text NOT NULL,
+  `county_id` int(11) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` int(1) NOT NULL DEFAULT 0,
+  `deleted` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `ratings` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `ratinged_user_id` int(11),
-  `ratinger_user_id` int(11),
-  `desc` text,
-  `ratings_stars` int(2),
-  `status` int(1)
-);
+-- --------------------------------------------------------
 
+--
+-- Tábla szerkezet ehhez a táblához `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL,
+  `postal_code` int(5) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `county_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `companies`
+--
+
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE `companies` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `premise_address` varchar(255) NOT NULL,
+  `tax_number` varchar(255) NOT NULL,
+  `deleted` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `counties`
+--
+
+DROP TABLE IF EXISTS `counties`;
+CREATE TABLE `counties` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `favorites`
+--
+
+DROP TABLE IF EXISTS `favorites`;
 CREATE TABLE `favorites` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int(11),
-  `ads_id` int(11)
-);
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `ads_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `images`
+--
+
+DROP TABLE IF EXISTS `images`;
 CREATE TABLE `images` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `url` varchar(255),
-  `title` varchar(100),
-  `created_at` date,
-  `user_id` int(11)
-);
+  `id` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `created_at` date NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `ads` ADD FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `ads` (`user_id`);
+--
+-- Tábla szerkezet ehhez a táblához `jobs`
+--
 
-ALTER TABLE `job_tags` ADD FOREIGN KEY (`id`) REFERENCES `ads` (`job_tag_id`);
+DROP TABLE IF EXISTS `jobs`;
+CREATE TABLE `jobs` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `desc` text NOT NULL,
+  `total` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT -1,
+  `worker_accepted` int(1) NOT NULL DEFAULT 0,
+  `customer_accepted` int(1) NOT NULL DEFAULT 0,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `counties` ADD FOREIGN KEY (`id`) REFERENCES `ads` (`county_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `ratings` (`ratinged_user_id`);
+--
+-- Tábla szerkezet ehhez a táblához `job_tags`
+--
 
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `ratings` (`ratinger_user_id`);
+DROP TABLE IF EXISTS `job_tags`;
+CREATE TABLE `job_tags` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `job_tags` ADD FOREIGN KEY (`id`) REFERENCES `users` (`job_tag_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `favorites` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+--
+-- Tábla szerkezet ehhez a táblához `messages`
+--
 
-ALTER TABLE `favorites` ADD FOREIGN KEY (`ads_id`) REFERENCES `ads` (`id`);
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `checked` int(1) NOT NULL DEFAULT 0,
+  `sended_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `images` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `cities` ADD FOREIGN KEY (`county_id`) REFERENCES `counties` (`id`);
+--
+-- Tábla szerkezet ehhez a táblához `ratings`
+--
 
-ALTER TABLE `streets` ADD FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`);
+DROP TABLE IF EXISTS `ratings`;
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL,
+  `ratinged_user_id` int(11) NOT NULL,
+  `ratinger_user_id` int(11) NOT NULL,
+  `desc` text NOT NULL,
+  `ratings_stars` int(2) NOT NULL DEFAULT 0,
+  `status` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `streets` ADD FOREIGN KEY (`street_name_id`) REFERENCES `street_names` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `addresses` ADD FOREIGN KEY (`street_id`) REFERENCES `streets` (`id`);
+--
+-- Tábla szerkezet ehhez a táblához `streets`
+--
 
-ALTER TABLE `addresses` ADD FOREIGN KEY (`id`) REFERENCES `users` (`address_id`);
+DROP TABLE IF EXISTS `streets`;
+CREATE TABLE `streets` (
+  `id` int(11) NOT NULL,
+  `street_name_id` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `companies` ADD FOREIGN KEY (`id`) REFERENCES `users` (`company_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `jobs` (`customer_id`);
+--
+-- Tábla szerkezet ehhez a táblához `street_names`
+--
 
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `jobs` (`worker_id`);
+DROP TABLE IF EXISTS `street_names`;
+CREATE TABLE `street_names` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `messages` (`sender_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `messages` (`receiver_id`);
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `acces_type` int(1) NOT NULL DEFAULT 0,
+  `email` varchar(200) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `company_id` int(11) DEFAULT NULL,
+  `job_tag_id` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT -1,
+  `last_login_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `activated_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `address_id` int(11) NOT NULL,
+  `deleted` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `counties`
+--
+ALTER TABLE `counties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `job_tags`
+--
+ALTER TABLE `job_tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `streets`
+--
+ALTER TABLE `streets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `street_names`
+--
+ALTER TABLE `street_names`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `ads`
+--
+ALTER TABLE `ads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `counties`
+--
+ALTER TABLE `counties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `favorites`
+--
+ALTER TABLE `favorites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `job_tags`
+--
+ALTER TABLE `job_tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `streets`
+--
+ALTER TABLE `streets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `street_names`
+--
+ALTER TABLE `street_names`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `ads`
+--
+ALTER TABLE `ads`
+  ADD CONSTRAINT `ads_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
