daf9e050399671d8027ad236df35f76409f47901
Isstrasa <102282558+Isstrasa@users.noreply.github.com>
2023-02-12 20:33:59 +0100

    Javítás

--- Frontend/szakemberKereso/src/app/_components/address-form/address-form.component.ts
+++ Frontend/szakemberKereso/src/app/_components/address-form/address-form.component.ts
@@ -11,26 +11,16 @@ import { Tag } from 'src/app/_model/Tag';
 export class AddressFormComponent implements OnInit {
 
   @Input() addressFormGroup!: FormGroup;
-  cities: Tag[] = [];
-  counties: Tag[] = [];
-  streetsNames: Tag[] = [];
+  @Input() cities: Tag[] = [];
+  @Input() counties: Tag[] = [];
+  @Input() streetsNames: Tag[] = [];
   @Input() addressFields!: Field[];
-  // @Input() city!: FormControl;
-  // @Input() county!: FormControl;
-  // @Input() streetName!: FormControl;
+  @Input() city!: FormControl;
+  @Input() county!: FormControl;
+  @Input() streetName!: FormControl;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.addressFormGroup = new FormGroup({
-      zipCode: new FormControl(''),
-      county: new FormControl(''),
-      city: new FormControl(''),
-      streetName: new FormControl(''),
-      number: new FormControl(''),
-      staircase: new FormControl(''),
-      floor: new FormControl(''),
-      door: new FormControl('')
-    })
   }
 }
