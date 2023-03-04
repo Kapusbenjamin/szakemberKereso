c5378e289b357038fddd19e1b0d7810e03bfe474
Isstrasa <102282558+Isstrasa@users.noreply.github.com>
2023-03-04 09:51:34 +0100

    chat scroll down

--- Frontend/szakemberKereso/src/app/_components/chat/chat.component.ts
+++ Frontend/szakemberKereso/src/app/_components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Chat } from 'src/app/_model/Chat';
 import { MessagesService } from 'src/app/_services/messages.service';
 import { UsersService } from 'src/app/_services/users.service';
@@ -8,13 +8,16 @@ import { UsersService } from 'src/app/_services/users.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
-
+export class ChatComponent implements OnInit, AfterViewChecked {
+  @ViewChild('scrollMe') private myScrollContainer!: ElementRef;
   @Input() chat!:any;
   messages:any[] = [];
   userId!:number;
 
   constructor(private userService: UsersService, private messagesService: MessagesService) { }
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
 
   ngOnInit(): void {
     this.userId = this.userService.userData.userId;
@@ -27,6 +30,12 @@ export class ChatComponent implements OnInit {
     this.messagesService.getAllMessagesBetweenUsers(chat.senderId,chat.receiverId).subscribe((messages: any)=>{
       this.messages = messages;
     })
+  }
+
+  scrollToBottom(): void {
+    try {
+        this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch(err) { }
   }
 
 }
